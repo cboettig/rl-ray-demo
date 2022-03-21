96affbbc1b2cb94af9a060fb8ff15e98e59cb678
Carl <cboettig@gmail.com>
2022-03-21 19:43:12 +0000

    richer lstm

--- fishing.sh
+++ fishing.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-rllib train  --checkpoint-at-end -f fishing-PPO-config.yml
+rllib train -f fishing-PPO-config.yml
+rllib train -f fishing-ARS-config.yml
 
 rllib evaluate saved_checkpoint/checkpoint/checkpoint --episodes 10  --run PPO 
